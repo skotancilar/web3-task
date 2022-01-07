321f3abefd3a5534642f5e976f5cc581b468dce5
skotancilar <skotancilar@gmail.com>
2022-01-07 10:42:08 +0300

    token transfer events fetched

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import {useEffect, useState} from 'react';
 import './App.css';
+import {fetchTransfers} from './api';
+import {AxiosResponse} from 'axios';
 
 function App() {
-  return (
-    <div className="bg-black text-white text-center">
-      Web3 Task
-    </div>
-  );
+	const [transfers, setTransfers] = useState([]);
+
+	useEffect(() => {
+		fetchTransfers.then((res: AxiosResponse) => {
+			setTransfers(res.data.result);
+			console.log(transfers);
+		});
+	}, [transfers]);
+	return (
+		<div className='bg-black mr-[2.2px] text-white text-center'>
+			PLACEHOLDER
+		</div>
+	);
 }
 
 export default App;
