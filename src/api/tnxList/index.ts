48da301bd3a9e214612ae8e33fe75bd4eaab0e32
skotancilar <skotancilar@gmail.com>
2022-01-08 19:30:33 +0300

    tnx list api corrected

--- src/api/tnxList/index.ts
+++ src/api/tnxList/index.ts
@@ -6,9 +6,10 @@ const api = axios.create({baseURL: 'https://api.bscscan.com/api'});
 
 export const tnxList = api.get(`?module=account
 &action=txlist
-&address=0xee62650fa45ac0deb1b24ec19f983a8f85b727ab&startblock=0
+&address=${REACT_APP_ADDRESS}
+&startblock=0
 &endblock=99999999
 &page=1
 &offset=500
 &sort=asc
-&apikey=V49F8RQAREENQDCFZS182JMJ27BAX789BD`);
+&apikey=${REACT_APP_API_KEY}`);
